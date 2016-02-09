758c36a2150ed451a33acd09f7e719bf7e4fc186
Vincent Rolea <vincent.rolea@gmail.com>
2016-02-09 12:03:18 +0100

    updated routing file

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,14 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # Lire toutes les todos
+  get 'todos', to: 'todos#index'
+  # Lire une todo particulière
+  get 'todos/:id', to: 'todos#show'
+  # ajouter une todo
+  get 'todos/new', to: 'todos#new'
+  post 'todos', to: 'todos#create'
+  # éditer une todo
+  get 'todos/:id/edit', to: 'todos#edit'
+  patch 'todos/:id', to: 'todos#update'
+  # détruire une todo
+  delete 'todos/:id', to: 'todos#destroy'
 end
